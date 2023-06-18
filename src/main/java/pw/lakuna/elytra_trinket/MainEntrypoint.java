8d198625f39be4bdfc0c45d7660cfde5ba362990
Travis Martin <tjmartin2003@gmail.com>
2023-06-18 17:12:59 -0500

    Enable flight when an Elytra is equipped as a trinket.

--- src/main/java/pw/lakuna/elytra_trinket/MainEntrypoint.java
+++ src/main/java/pw/lakuna/elytra_trinket/MainEntrypoint.java
@@ -1,10 +1,15 @@
 package pw.lakuna.elytra_trinket;
 
+import dev.emi.trinkets.api.TrinketsApi;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.entity.LivingEntity;
+import net.fabricmc.fabric.api.entity.event.v1.EntityElytraEvents;
+import net.minecraft.item.Items;
 
 public final class MainEntrypoint implements ModInitializer {
 	@Override
 	public void onInitialize() {
+		EntityElytraEvents.CUSTOM.register((LivingEntity entity, boolean tickElytra) -> TrinketsApi.getTrinketComponent(entity).get().isEquipped(Items.ELYTRA));
 		ElytraTrinket.logger.info("Initialized Elytra Trinket (main).");
 	}
 }
