807ae9b89a92d4ab6776a85f7d82d035e27e92ce
Travis Martin <tjmartin2003@gmail.com>
2023-06-18 00:56:56 -0500

    Split client and server code.

--- src/main/java/pw/lakuna/elytra_trinket/ElytraTrinket.java
+++ src/main/java/pw/lakuna/elytra_trinket/ElytraTrinket.java
@@ -4,5 +4,5 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public final class ElytraTrinket {
-	public static final Logger logger = LoggerFactory.getLogger("elytra_trinket");
+	public static final Logger logger = LoggerFactory.getLogger("Elytra Trinket");
 }
